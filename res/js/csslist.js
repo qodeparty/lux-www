163b77d18cf8e7c4c48b2ddb21f64bac5f29398c
qodebot <30004919+qodebot@users.noreply.github.com>
2018-07-21 01:50:49 +0000

    auto build 122.1532137849 :smile:

--- res/js/csslist.js
+++
@@ -1,8 +0,0 @@
-//generated 4
-var csslist = [
-'lux-debug.css',
-'lux-elements.css',
-'lux-equalizer.css',
-'lux-layout.css',
-'lux-modifier.css'
-];
--- res/js/jslist.js
+++
@@ -1,7 +0,0 @@
-//generated 3
-var jslist = [
-'csslist.js',
-'htmllist.js',
-'jslist.js',
-'lux-helper.js'
-];
