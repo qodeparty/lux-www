a8592014d92a0b0b501f92084a97c82c32834c4f
qodebot <30004919+qodebot@users.noreply.github.com>
2018-08-02 21:51:23 +0000

    welp stuff

--- res/js/lux-meta.js
+++ res/js/lux-meta.js
@@ -1,10 +1,9 @@
-//// lux generated javascript file Fri Jul 27 23:10:30 UTC 2018
+//// lux generated javascript file Thu Aug  2 21:13:06 UTC 2018
 const LUX_VERSION="v0.8.6"
-const LUX_BUILD="150"
+const LUX_BUILD="152"
 const LUX_BASIS=""
 const LUX_THEME="archxray"
-const LUX_TIMESTAMP="1532733030"
-
+const LUX_TIMESTAMP="1533244386"
 ////
 
 ////found 8 js files
